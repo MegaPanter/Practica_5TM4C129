e4de8a56ea7bcf22f83cd4aeec438fa1f9375fa1
MegaPanter <113056259+MegaPanter@users.noreply.github.com>
2023-01-19 16:27:54 -0600

    epale

--- main.c
+++ main.c
@@ -7,9 +7,10 @@ int main(void)
     volatile uint16_t freq;
      freq=1250;
 
-    uint16_t Result[2];
-    uint16_t duty[2];
-    Configurar_PLL();  //Confiuracion de velocidad de reloj
+    uint16_t Result[3];
+    uint16_t duty[3];
+    Configurar_PLL();  
+    //Confiuracion de velocidad de reloj
     Configura_Reg_ADC0();
     Configurar_UART0();
     Configura_Reg_PWM1(50);//Configuro a 1khz el pwm
@@ -22,7 +23,10 @@ int main(void)
        
     ADC0_InSeq2(Result,duty);
         PWM0->_1_CMPA = duty[1];
-                
+        delayms(10);
+        PWM0->_1_CMPB = duty[2];
+        delayms(10);
+        PWM0->_0_CMPA = duty[0];          
       
        
 }
