4ba3efe07fb21848281e58c597c5ec9f3493dbe8
MegaPanter <113056259+MegaPanter@users.noreply.github.com>
2023-01-29 15:43:04 -0600

    PWM COMENTADO

--- main.c
+++ main.c
@@ -7,28 +7,27 @@ int main(void)
     volatile uint16_t freq;
      freq=1250;
 
-    uint16_t Result[2];
-    uint16_t duty[2];
-    Configurar_PLL();  //Confiuracion de velocidad de reloj
+    uint16_t Result[3];
+    uint16_t duty[3];
+    Configurar_PLL();  
     Configura_Reg_ADC0();
     Configurar_UART0();
-    Configura_Reg_PWM1(1250);//Configuro a 1khz el pwm
+    Configura_Reg_PWM1(50);
+
+
     while(1)
     {
          //llamada a la conversion por procesador
-       for(x=159;x>=1;x--)
-       {
-        PWM0->_1_CMPA = (x*100);
-        delayms(20);
-       }
-       for(x=1;x<=159;x--)
-       {
-         PWM0->_1_CMPA = x*100;
-         delayms(20);
-       }
-       delayms(50);
-
-
-}
+     
+       
+    ADC0_InSeq2(Result,duty);
+        PWM0->_1_CMPA = duty[1];
+        delayms(10);
+        PWM0->_1_CMPB = duty[2];
+        delayms(10);
+        PWM0->_0_CMPA = duty[0];          
+      
+       
 }
 
+}
